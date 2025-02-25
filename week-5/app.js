00bd0012658efea8aebbfdfa8f8a978136e3e04b
Rhoda <rhodachela@gmail.com>
2025-02-25 23:45:38 +0300

    Introduction to Javascript

---
+++ week-5/app.js
@@ -0,0 +1,15 @@
+console.log("Hello, my name is Chela")
+console.log("I a learning JS today")
+console.log("Doremifasoladidooooo!!")
+console.log(5000 * 0.00008)
+x = 500
+y = 800
+result = x + y
+console.log(result)
+
+var x = "Rhoda"
+let MyStatus = "Single";
+let fun = document.getElementById("heading")
+fun.textContent = MyStatus
+console.log(MyStatus)
+
